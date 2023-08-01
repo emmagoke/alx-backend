c70ce2965ff5b5b580dcfd3c09bf6c0427e5cbfd
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-02 00:26:53 +0100

    Create the home route in flask, with babel configuration

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This is a flask app that impliments simple Internationalization 
+This is a flask app that impliments simple Internationalization
 """
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 
 class Config(object):
-    
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 # Using Config class to configure this flask app
 app.config.from_object(Config)
@@ -22,9 +23,9 @@ babel = Babel(app)
 
 @app.route('/', strict_slashes=False)
 def home():
-	""" The home route. """
-	return render_template('1-index.html')
+    """ The home route. """
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
