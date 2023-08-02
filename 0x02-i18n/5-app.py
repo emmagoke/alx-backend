1ea32b024217733ef5ad7333b5470971c3c442d2
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-02 04:37:25 +0100

    Added task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+This is a flask app that handles search query
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """
+    This class is used for the configuration of the flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.before_request
+def get_user():
+    """ This function get the login in user. """
+    user = request.args.get('login_as')
+    if user:
+        if int(user) in users:
+            user_detail = users.get(int(user))
+            g.user = user_detail
+    else:
+        g.user = None
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    This function determine the best match with local supported languages.
+    """
+    query = request.args.get('locale')
+    if query:
+        if query in app.config['LANGUAGES']:
+            return query
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """ This is the home route.  """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
