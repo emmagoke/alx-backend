d38c26c9579c7d394dba526fb3b929e62c063b2b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-02 03:48:14 +0100

    Create the home route in flask, with babel configuration and a function to pick the best local language

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,7 +23,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
     This function determine the best match with local supported languages.
     """
@@ -31,7 +31,7 @@ def get_locale():
 
 
 @app.route('/', )
-def home():
+def home() -> str:
     """ This is the home route. """
     return render_template('2-index.html')
 
