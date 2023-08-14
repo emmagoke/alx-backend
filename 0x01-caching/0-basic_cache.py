1d5967bfcbdebc7c7849623638ec3cb678c50390
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-14 09:03:12 +0100

    Created the BasicCache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This script contins a simple cache implemented as a python Class.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    This is a Basic Cache Implementationin Python.
+    """
+
+    def put(self, key, item):
+        """
+        This method assigns key and value to the dictionary
+        """
+        if key or item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        This method return the value of a key in a dictionary
+        """
+        if key:
+            value = self.cache_data.get(key)
+            if value:
+                return value
+            return None
+        return None
