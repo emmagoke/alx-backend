1835d8af7274c6bd61e82d63e51d8454bac954d2
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-09-01 21:43:09 +0100

    Added part solution to question 12

---
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+const port = 1245;
+
+const listProducts = [
+  { Id: 1, name: "Suitcase 250", price: 50, stock: 4 },
+  { Id: 2, name: "Suitcase 450", price: 100, stock: 10 },
+  { Id: 3, name: "Suitcase 650", price: 350, stock: 2 },
+  { Id: 4, name: "Suitcase 1050", price: 550, stock: 5 },
+];
+
+function getItemById(id) {}
